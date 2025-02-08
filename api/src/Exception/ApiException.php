a9f838cc804f80fbfb93979633f8bfac9b915d44
Saman Esmaeil <saman.2791@gmail.com>
2025-02-09 02:31:28 +0330

    fix: event handler wip

---
+++ api/src/Exception/ApiException.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Exception;
+
+use Symfony\Component\HttpKernel\Exception\HttpException;
+
+class ApiException extends HttpException
+{
+    private $details;
+
+    public function __construct(int $statusCode, string $message = '', array $details = [], \Throwable $previous = null, array $headers = [])
+    {
+        $this->details = $details;
+        parent::__construct($statusCode, $message, $previous, $headers, $statusCode);
+    }
+
+    public function getDetails(): array
+    {
+        return $this->details;
+    }
+}
